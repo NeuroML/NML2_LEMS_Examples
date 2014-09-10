8c6bc6ee1359118d070d5c8ad9c0243f37ff6c80
Padraig Gleeson <p.gleeson@gmail.com>
2014-09-10 20:03:53 +0100

    Updating windows batch file

--- lems.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set LEMS_VERSION=0.9.5.3
-
-set CLASSPATH=.;.\target\jlems%LEMS_VERSION%-.jar;%LEMS_HOME%\target\jlems-%LEMS_VERSION%.jar
-
-echo Running the LEMS application...
-
-java -Xmx400M -classpath %CLASSPATH% org.lemsml.jlems.viz.VizMain %1 %2 %3 %4
